8832b4d9fedde79bc77b469548f9d6d02936fda2
NTS-MEGHARATHOD\megha <megha.rathod@neutrinotechlabs.com>
2022-06-11 14:49:20 +0530

    megha1106

--- src/main/java/Utilities/API_Utils.java
+++ src/main/java/Utilities/API_Utils.java
@@ -1,11 +1,16 @@
 package Utilities;
 
+import io.restassured.RestAssured;
+import io.restassured.config.EncoderConfig;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+
+import static io.restassured.RestAssured.given;
 
 public class API_Utils {
     public static Properties configs;
@@ -14,26 +19,47 @@ public class API_Utils {
     public static Response response;
 
     public static void readAPIConfigs() throws IOException {
+        System.out.println("inside readAPIConfigs");
         configs = new Properties();
-        FileInputStream fs = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\resources\\configs.properties");
+        FileInputStream fs = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\resources\\configs\\config.properties");
+        System.out.println("after readAPIConfigs");
         configs.load(fs);
-
+        System.out.println("after load readAPIConfigs");
     }
 
-    public static Response postRequest(String key, String value) {
-        request.header("Content-Type", "application/json");
-        request.body(value);
-        response =request.post(key);
-
-        return response;
-}
-    public static Response getRequest(String key) {
-        request.header("Content-Type", "application/json");
-        response =request.get(key);
-        return response;
-
-
+    public static void setBaseURL() throws IOException {
+        RestAssured.baseURI = configs.getProperty("base_url");
     }
+
+
+    public static Response getRequest(String endpoint) {
+        RestAssured.given();
+        request.header("Content-Type", "application/json");
+        response = request.get(endpoint);
+        return response;
+    }
+
+//    private static String getToken() {
+//        //make an auth API call to get token if available
+//        String token = API_Utils.configs.getProperty("access_token");
+//        return token;
+//    }
+
+    public static Response postRequest(String endpoint, String formdata) {
+        request = RestAssured.given();
+        request.header("Content-type", "application/json");
+        request.body(formdata);
+        response = request.post(endpoint);
+        return response;
+    }
+
+//    public static Response postRequest1(String endpoint, String formdata) {
+//        return given()
+//                .contentType(ContentType.JSON).
+//                config(RestAssured.config().encoderConfig(EncoderConfig.encoderConfig().encodeContentTypeAs("ContentType.JSON", ContentType.TEXT))).
+//                body(formdata).
+//                post(endpoint);
+//    }
 
 
 }
--- src/test/java/TestCases/GetRequestTest.java
+++ src/test/java/TestCases/GetRequestTest.java
@@ -1,25 +1,41 @@
 package TestCases;
 
+import Utilities.API_Utils;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 
 public class GetRequestTest {
+
+    String endpoint;
+    @BeforeClass
+    public void startUp() throws IOException {
+        API_Utils.readAPIConfigs();
+        API_Utils.setBaseURL();
+    }
+
     @Test
     public void getAllUsers(){
 
-
+        System.out.println("inside get method");
+        endpoint=API_Utils.configs.getProperty("get_user_end_point");
+        System.out.println("endpoint is ="+endpoint);
+        Response response=API_Utils.getRequest(endpoint);
+        System.out.println("after response in vertical="+response.asPrettyString());
 //        specify base URI:
-        RestAssured.baseURI = "https://reqres.in/";
+//        RestAssured.baseURI = "https://reqres.in/";
 
 //                Represents request object
-        RequestSpecification httprequest = RestAssured.given();
+//        RequestSpecification httprequest = RestAssured.given();
 //                response object
-        Response response = httprequest.request(Method.GET, "/api/users?page=2");
+//        Response response = httprequest.request(Method.GET, "/api/users?page=2");
 //                print response in console
         String responseBody = response.getBody().asString();
         System.out.println("Response Body is=" + responseBody);
